5643f5568f08a694f935e8ede5c9c32070185b0e
David Négrier <d.negrier@thecodingmachine.com>
2019-04-23 20:48:03 +0200

    Adding support for JSON serialization annotations

---
+++ src/TdbmFluidColumnJsonOptions.php
@@ -0,0 +1,154 @@
+<?php
+
+
+namespace TheCodingMachine\FluidSchema;
+
+
+use function addslashes;
+use Doctrine\DBAL\Types\Type;
+use function var_export;
+
+class TdbmFluidColumnJsonOptions
+{
+    /**
+     * @var TdbmFluidColumnOptions
+     */
+    private $tdbmFluidColumnOptions;
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var string
+     */
+    private $outputType;
+    /**
+     * @var FluidColumn
+     */
+    private $fluidColumn;
+
+    public function __construct(TdbmFluidColumnOptions $tdbmFluidColumnOptions, FluidColumn $fluidColumn)
+    {
+        $this->tdbmFluidColumnOptions = $tdbmFluidColumnOptions;
+        $this->fluidColumn = $fluidColumn;
+    }
+
+    private function getComment(): Comment
+    {
+        $comment = $this->fluidColumn->getDbalColumn()->getComment();
+
+        return new Comment($comment ?? '');
+    }
+
+    public function key(string $name): self
+    {
+        $this->addAnnotation('JsonKey', ['key' => $name]);
+        return $this;
+    }
+
+    public function datetimeFormat(string $format): self
+    {
+        $this->addAnnotation('JsonFormat', ['date' => $format]);
+        return $this;
+    }
+
+    /**
+     * @param int|null $decimals The number of decimals
+     * @param string|null $point The decimal point
+     * @param string|null $separator The thousands separator
+     * @param string|null $unit The suffix to append after a number
+     */
+    public function numericFormat(?int $decimals = null, ?string $point = null, ?string $separator = null, ?string $unit = null): self
+    {
+        $params = [];
+        if ($decimals !== null) {
+            $params['decimals'] = $decimals;
+        }
+        if ($point !== null) {
+            $params['point'] = $point;
+        }
+        if ($separator !== null) {
+            $params['separator'] = $separator;
+        }
+        if ($unit !== null) {
+            $params['unit'] = $unit;
+        }
+        $this->addAnnotation('JsonFormat', $params);
+        return $this;
+    }
+
+    /**
+     * Serialize in JSON by getting the property from an object.
+     */
+    public function formatUsingProperty(string $property): self
+    {
+        $this->addAnnotation('JsonFormat', ['property' => $property]);
+        return $this;
+    }
+
+    /**
+     * Serialize in JSON by calling a method from an object.
+     */
+    public function formatUsingMethod(string $method): self
+    {
+        $this->addAnnotation('JsonFormat', ['method' => $method]);
+        return $this;
+    }
+
+    public function ignore(): self
+    {
+        $this->addAnnotation('JsonIgnore');
+        return $this;
+    }
+
+    public function include(): self
+    {
+        $this->addAnnotation('JsonInclude');
+        return $this;
+    }
+
+    public function recursive(): self
+    {
+        $this->addAnnotation('JsonRecursive');
+        return $this;
+    }
+
+    public function collection(string $key): self
+    {
+        $this->addAnnotation('JsonCollection', ['key' => $key]);
+        return $this;
+    }
+
+    /**
+     * @param string $annotation
+     * @param mixed $content
+     * @param bool $replaceExisting
+     * @return TdbmFluidColumnGraphqlOptions
+     */
+    public function addAnnotation(string $annotation, $content = null, bool $replaceExisting = true, bool $explicitNull = false): self
+    {
+        $this->tdbmFluidColumnOptions->addAnnotation($annotation, $content, $replaceExisting, $explicitNull);
+        return $this;
+    }
+
+    public function removeAnnotation(string $annotation): self
+    {
+        $this->tdbmFluidColumnOptions->removeAnnotation($annotation);
+        return $this;
+    }
+
+    public function endJsonSerialize(): TdbmFluidColumnOptions
+    {
+        return $this->tdbmFluidColumnOptions;
+    }
+
+    public function then(): TdbmFluidTable
+    {
+        return $this->tdbmFluidColumnOptions->then();
+    }
+
+    public function column(string $name): TdbmFluidColumn
+    {
+        return $this->tdbmFluidColumnOptions->column($name);
+    }
+}
---
+++ tests/TdbmFluidJunctionTableJsonOptionsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace TheCodingMachine\FluidSchema;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
+use PHPUnit\Framework\TestCase;
+
+class TdbmFluidJunctionTableJsonOptionsTest extends TestCase
+{
+    public function testJson()
+    {
+        $schema = new Schema();
+        $fluid = new TdbmFluidSchema($schema);
+
+        $fluid->table('nodes')
+            ->column('id')->integer()->primaryKey()->autoIncrement()->jsonSerialize()->ignore()
+            ->column('alias_id')->references('nodes')->null()->jsonSerialize()->recursive()
+            ->column('parent_id')->references('nodes')->null()->jsonSerialize()->include()
+            ->column('root_id')->references('nodes')->null()->jsonSerialize()->ignore()
+            ->column('owner_id')->references('nodes')->null()->jsonSerialize()->formatUsingProperty('name')->include()
+            ->column('name')->string()->jsonSerialize()->key('basename')
+            ->column('size')->integer()->notNull()->default(0)->jsonSerialize()->numericFormat(null, null, null, ' o')
+            ->column('weight')->float()->null()->jsonSerialize()->numericFormat(2, ',', '.', 'g')
+            ->column('created_at')->date()->null()->jsonSerialize()->datetimeFormat("Y-m-d")
+            ->column('another_parent')->references('nodes')->comment('@JsonCollection("entries") @JsonFormat(property="entry")');
+
+        $nodesTable = $schema->getTable('nodes');
+        $this->assertContains("@JsonIgnore", $nodesTable->getColumn('id')->getComment());
+        $this->assertContains("@JsonRecursive", $nodesTable->getColumn('alias_id')->getComment());
+        $this->assertContains("@JsonInclude", $nodesTable->getColumn('parent_id')->getComment());
+        $this->assertContains("@JsonIgnore", $nodesTable->getColumn('root_id')->getComment());
+        $this->assertContains('@JsonFormat(property = "name")', $nodesTable->getColumn('owner_id')->getComment());
+        $this->assertContains("@JsonKey(key = \"basename\")", $nodesTable->getColumn('name')->getComment());
+        $this->assertContains("@JsonFormat(unit = \" o\")", $nodesTable->getColumn('size')->getComment());
+        $this->assertContains("@JsonFormat(decimals = 2, point = \",\", separator = \".\", unit = \"g\")", $nodesTable->getColumn('weight')->getComment());
+        $this->assertContains("@JsonFormat(date = \"Y-m-d\")", $nodesTable->getColumn('created_at')->getComment());
+        $this->assertContains("@JsonCollection(\"entries\")", $nodesTable->getColumn('another_parent')->getComment());
+    }
+}
