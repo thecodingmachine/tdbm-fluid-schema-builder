7653d3bce775cd00bf59064b36dd71a33253310a
David Négrier <d.negrier@thecodingmachine.com>
2019-04-23 20:54:11 +0200

    Adding some documentation

--- tests/TdbmFluidJunctionTableJsonOptionsTest.php
+++ tests/TdbmFluidJunctionTableJsonOptionsTest.php
@@ -19,6 +19,7 @@ class TdbmFluidJunctionTableJsonOptionsTest extends TestCase
             ->column('parent_id')->references('nodes')->null()->jsonSerialize()->include()
             ->column('root_id')->references('nodes')->null()->jsonSerialize()->ignore()
             ->column('owner_id')->references('nodes')->null()->jsonSerialize()->formatUsingProperty('name')->include()
+            ->column('owner_country')->references('nodes')->null()->jsonSerialize()->formatUsingMethod('myMethod')->include()
             ->column('name')->string()->jsonSerialize()->key('basename')
             ->column('size')->integer()->notNull()->default(0)->jsonSerialize()->numericFormat(null, null, null, ' o')
             ->column('weight')->float()->null()->jsonSerialize()->numericFormat(2, ',', '.', 'g')
@@ -31,10 +32,14 @@ class TdbmFluidJunctionTableJsonOptionsTest extends TestCase
         $this->assertContains("@JsonInclude", $nodesTable->getColumn('parent_id')->getComment());
         $this->assertContains("@JsonIgnore", $nodesTable->getColumn('root_id')->getComment());
         $this->assertContains('@JsonFormat(property = "name")', $nodesTable->getColumn('owner_id')->getComment());
+        $this->assertContains('@JsonFormat(method = "myMethod")', $nodesTable->getColumn('owner_country')->getComment());
         $this->assertContains("@JsonKey(key = \"basename\")", $nodesTable->getColumn('name')->getComment());
         $this->assertContains("@JsonFormat(unit = \" o\")", $nodesTable->getColumn('size')->getComment());
         $this->assertContains("@JsonFormat(decimals = 2, point = \",\", separator = \".\", unit = \"g\")", $nodesTable->getColumn('weight')->getComment());
         $this->assertContains("@JsonFormat(date = \"Y-m-d\")", $nodesTable->getColumn('created_at')->getComment());
         $this->assertContains("@JsonCollection(\"entries\")", $nodesTable->getColumn('another_parent')->getComment());
+
+        $anotherColumn = $fluid->table('nodes')->column('another_column')->integer();
+        $this->assertSame($anotherColumn, $anotherColumn->jsonSerialize()->endJsonSerialize());
     }
 }
